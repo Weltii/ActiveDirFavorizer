39e98600b584fab5a226993958893f5749c53f0d
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2020-05-09 14:25:31 +0200

    Add the ThunarBookmarkManager
    The ThunarBookmarkManager handle the bookmarks under /home/user/.config/gtk-3.0/bookmarks

---
+++ test_thunar_bookmark_manager.py
@@ -0,0 +1,71 @@
+import pytest
+
+from Path import Path, Flags
+from thunar_bookmark_manager import ThunarBookmarkManager
+
+
+def read_file(self):
+	return ["/home/user/top_of_a_table", "/home/user/table_top"]
+
+
+def write_file(self, content: str):
+	return content
+
+
+def generate_thunar_manager():
+	# overwrite the file system operations
+	ThunarBookmarkManager.read_file = read_file
+	ThunarBookmarkManager.write_file = write_file
+	manager = ThunarBookmarkManager()
+	return manager
+
+
+def test_read_file():
+	manager = generate_thunar_manager()
+	assert manager.read_file() == ["/home/user/top_of_a_table", "/home/user/table_top"]
+
+
+def test_write_file():
+	manager = generate_thunar_manager()
+	manager.add_bookmark(Path("home/user/dungeons_dungeons_and_more_dungeons", Flags.NORMAL), "")
+	assert manager.write_file(manager.paths_to_bookmark(manager.paths)) == "/home/user/top_of_a_table\n/home/user/table_top\nhome/user" \
+																 "/dungeons_dungeons_and_more_dungeons\n"
+
+
+def test_load_bookmarks():
+	manager = generate_thunar_manager()
+	paths = manager.load_bookmarks(Flags.LOCKED)
+	expected_paths = [
+		Path("/home/user/top_of_a_table", Flags.LOCKED),
+		Path("/home/user/table_top", Flags.LOCKED)
+	]
+	# TODO test the content of both lists, at the moment is it really imprecisely
+	assert len(paths) == len(expected_paths)
+	expected_paths.append(Path("/home/user/dungeons_dungeons_and_more_dungeons", Flags.LOCKED))
+	assert len(paths) != len(expected_paths)
+
+
+def test_paths_to_bookmark():
+	manager = generate_thunar_manager()
+	assert manager.paths_to_bookmark(manager.paths) == "/home/user/top_of_a_table\n/home/user/table_top\n"
+
+
+def test_remove_bookmark():
+	manager = generate_thunar_manager()
+	manager.add_bookmark(Path("/home/user/dungeons_and_dragons", Flags.NORMAL), "")
+	assert len(manager.paths) == 3
+	manager.remove_bookmark(Path("/home/user/dungeons_and_dragons", Flags.NORMAL), "")
+	assert len(manager.paths) == 2
+	manager.remove_bookmark(Path("/home/user/top_of_a_table", Flags.NORMAL), "")
+	assert len(manager.paths) == 2
+
+
+def test_add_bookmark():
+	manager = generate_thunar_manager()
+	assert len(manager.paths) == 2
+	manager.add_bookmark(Path("/home/user/dungeons_and_dragons", Flags.NORMAL), "")
+	assert len(manager.paths) == 3
+	manager.add_bookmark(Path("/home/user/dungeons_and_dragons", Flags.NORMAL), "")
+	assert len(manager.paths) == 3
+
+
---
+++ thunar_bookmark_manager.py
@@ -0,0 +1,70 @@
+from typing import List
+from Path import Flags, Path
+from BookmarkManager import BookmarkManager
+import getpass
+bookmark_path = f"/home/{getpass.getuser()}/.config/gtk-3.0/bookmarks"
+
+
+class ThunarBookmarkManager(BookmarkManager):
+	def read_file(self):
+		file = open(bookmark_path, "r")
+		return file.readlines()
+
+	def write_file(self, content: str):
+		file = open(bookmark_path, "w")
+		file.write(content)
+		file.close()
+
+	def load_bookmarks(self, flag=Flags.NORMAL):
+		bookmarks = self.read_file()
+		paths = []
+
+		for path in bookmarks:
+			paths.append(Path(path, flag))
+		return paths
+
+	def paths_to_bookmark(self, paths: List[Path]):
+		val = ""
+		for path in paths:
+			val += f"{path.path}\n"
+		return val
+
+	def save_bookmarks(self, paths: List[Path]):
+		self.write_file(self.paths_to_bookmark(paths))
+
+	def save_backup(self):
+		self.save_bookmarks(self.paths_backup)
+
+	def remove_bookmark(self, path: Path, label: str):
+		found_path = None
+		for p in self.paths:
+			if p.path == path.path:
+				found_path = p
+
+		if found_path is not None:
+			if found_path.is_removable():
+				self.paths.remove(found_path)
+				self.save_bookmarks(self.paths)
+
+	def add_bookmark(self, path: Path, label: str):
+		if path is None or path.path is "":
+			return
+
+		not_found = True
+		for p in self.paths:
+			if p.path == path.path:
+				p.add_used_path(path.path)
+				not_found = False
+		if not_found:
+			self.paths.append(path)
+		self.save_bookmarks(self.paths)
+
+	def __init__(self):
+		self.paths_backup = self.load_bookmarks(Flags.LOCKED)
+		self.paths = self.paths_backup.copy()
+		# TODO create at the first startup a backup of the bookmarks file (bookmarks => bookmarks.bak)
+
+	def __del__(self):
+		pass
+		# TODO find a way to save the path backup if the program shutdown
+		# self.save_bookmarks(self.paths_backup)
